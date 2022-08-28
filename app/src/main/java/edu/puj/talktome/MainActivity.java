9e4045cd8d990315b334500f3f6a938d8aac4f02
andersonjalvarado <anderson__alvarado@hotmail.com>
2022-08-28 18:19:46 -0500

    Creacion

---
+++ app/src/main/java/edu/puj/talktome/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.puj.talktome;
+
+import androidx.appcompat.app.AppCompatActivity;
+import edu.puj.talktome.databinding.ActivityMainBinding;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
