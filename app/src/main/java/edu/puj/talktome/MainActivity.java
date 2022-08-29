db1fd99506ce0f1f25b57edf0e90b54eb36a0924
andersonjalvarado <anderson__alvarado@hotmail.com>
2022-08-28 20:15:22 -0500

    Home and Login

--- app/src/main/java/edu/puj/talktome/MainActivity.java
+++ app/src/main/java/edu/puj/talktome/MainActivity.java
@@ -2,13 +2,40 @@ package edu.puj.talktome;
 
 import androidx.appcompat.app.AppCompatActivity;
 import edu.puj.talktome.databinding.ActivityMainBinding;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(binding.getRoot());
+
+        //Animaciones
+        Animation animacion1 = AnimationUtils.loadAnimation(this,R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this,R.anim.desplazamiento_abajo);
+
+        binding.deTextView.setAnimation(animacion2);
+        binding.AndersonTextView.setAnimation(animacion2);
+        binding.logo.setAnimation(animacion1);
+        binding.nombre.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
     }
 }
