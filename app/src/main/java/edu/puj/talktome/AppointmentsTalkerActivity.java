89af81bc4a8b86cd7ab1281b7bf0100c9bb35733
Mauren <mauren.rivera.16@hotmail.com>
2022-08-30 16:34:26 -0500

    Se agregan vistas y navegación

---
+++ app/src/main/java/edu/puj/talktome/AppointmentsTalkerActivity.java
@@ -0,0 +1,14 @@
+package edu.puj.talktome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppointmentsTalkerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointments_talker);
+    }
+}
---
+++ app/src/main/java/edu/puj/talktome/AvailableProfessionalsActivity.java
@@ -0,0 +1,14 @@
+package edu.puj.talktome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvailableProfessionalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_available_professionals);
+    }
+}
