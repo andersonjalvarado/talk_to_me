3db6904b0c05bc3ae677e2754e97d0f1cb925fdb
Naruto360XL <sebastian@hotmail.com>
2022-08-31 10:47:27 -0500

    Layouts: History ManageAppointmentsDetailsProfessional PerfilProfesional NotificacionesTalker

---
+++ app/src/main/java/edu/puj/talktome/ManageAppointmentsDetailsProfessional.java
@@ -0,0 +1,14 @@
+package edu.puj.talktome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageAppointmentsDetailsProfessional extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_appointments_details_professional);
+    }
+}
