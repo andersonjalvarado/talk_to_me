423f9cffedd9c45ca67470efb7bf22cce847f79c
Mauren <mauren.rivera.16@hotmail.com>
2022-08-30 20:18:59 -0500

    Se agrega ubicaciones (maps)

--- app/src/main/java/edu/puj/talktome/LocationsTalkerActivity.java
+++ app/src/main/java/edu/puj/talktome/LocationsTalkerActivity.java
@@ -3,12 +3,36 @@ package edu.puj.talktome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class LocationsTalkerActivity extends AppCompatActivity {
+public class LocationsTalkerActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locations_talker);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(74.06475033068541, -74.06475033068541);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Universidad Javeriana"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
