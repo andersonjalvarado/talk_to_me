23ee1b61b329a6d5d03da711aad357eb79c153a6
Mauren <mauren.rivera.16@hotmail.com>
2022-08-31 14:48:38 -0500

    Flujo terminado

--- app/src/main/java/edu/puj/talktome/NotificacionesProfesionalActivity.java
+++ app/src/main/java/edu/puj/talktome/NotificacionesProfesionalActivity.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class NotificacionesTalkerActivity extends AppCompatActivity {
+public class NotificacionesProfesionalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notificaciones_talker);
+        setContentView(R.layout.activity_notificaciones_profesional);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, ScheduleCitaActivity.class));
     }
 
 }
